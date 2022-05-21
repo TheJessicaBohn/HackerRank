2815ffaf869ca1d365fd37a7dccce2af2f37a184
jessica bohn <jessicabohn74@gmail.com>
2022-05-20 22:26:02 -0300

    Add 4_A-Very-Big-Sum

---
+++ 4_A-Very-Big-Sum/Resolved_A-Very-Big-Sum.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static long aVeryBigSum(List<long> ar)
+    {
+        long n = 0;
+            
+        for(var i = 0; i < ar.Count; i++){
+            n = n + ar[i];
+        }    
+        
+        return n;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.aVeryBigSum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
